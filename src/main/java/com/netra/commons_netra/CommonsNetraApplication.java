e5f30057cf15cc82f7988c36bd45bc8f0d7e2f36
FEMI <oluwafemi.ayeni@interswitchgroup.com>
2025-07-12 23:20:38 +0100

    init project

---
+++ src/main/java/com/netra/commons_netra/CommonsNetraApplication.java
@@ -0,0 +1,13 @@
+package com.netra.commons_netra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonsNetraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonsNetraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netra/commons_netra/CommonsNetraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netra.commons_netra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsNetraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
